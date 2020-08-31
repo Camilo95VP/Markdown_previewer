fccb261abf5f50d97d18a3f09003a802f0c81d4d
JJuanC <camiloflstudio@gmail.com>
2020-08-31 14:00:34 -0500

    Updates

---
+++ precache-manifest.659ee7a0375ab0a4eef57a7d34caf2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71954d5e7116c6b319c84c64b847474",
+    "url": "/Camilo95VP/Markdown_previewer/index.html"
+  },
+  {
+    "revision": "16d5f0875b9fe330206c",
+    "url": "/Camilo95VP/Markdown_previewer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "16d5f0875b9fe330206c",
+    "url": "/Camilo95VP/Markdown_previewer/static/js/2.9ce836c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Camilo95VP/Markdown_previewer/static/js/2.9ce836c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea938a4c5cde38d1887f",
+    "url": "/Camilo95VP/Markdown_previewer/static/js/main.9e431431.chunk.js"
+  },
+  {
+    "revision": "a4fc54a9ccbc8986a4d8",
+    "url": "/Camilo95VP/Markdown_previewer/static/js/runtime-main.790d1c89.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Camilo95VP/Markdown_previewer/precache-manifest.659ee7a0375ab0a4eef57a7d34caf2e3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Camilo95VP/Markdown_previewer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
